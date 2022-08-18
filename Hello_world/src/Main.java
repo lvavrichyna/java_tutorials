b12a14c53b7a24c3430ef4851859e26ee84514cc
Liza Vavrichyna <lizavavrichyna@lizas-mbp.lan>
2022-08-18 12:47:47 -0500

    My first commit

---
+++ Hello_world/src/Main.java
@@ -0,0 +1,20 @@
+//to import library cmd+shift+O
+import javax.swing.JButton;
+
+import javax.swing.JSlider;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//create Hello world//
+		System.out.println("Hello world");
+		JButton button = new JButton();
+		JSlider slider = new JSlider();
+		
+		//sysout + cntr + space
+		System.out.print("Another print statement");
+		System.out.print("Keep printing in the same line");
+	}
+
+
+}
